dae603c9b148e813976ced937200df1a94bc6fae
xfear <joseluis.alfaix@gmail.com>
2023-12-12 21:01:21 +0100

    WIP for day1 after bug found

--- day1/main.cpp
+++ day1/main.cpp
@@ -39,23 +39,23 @@ int getSpelledDigit(size_t index, const std::string &line) {
 
 int getLineCalibration(const std::string &line) {
   size_t start = 0;
-  size_t end = line.size() <= 1 ? line.size() - 1 : 0;
+  size_t end = line.size() > 1 ? line.size() - 1 : 0; // Avoid blank or one-character lines
 
   int first_digit = 0;
   int second_digit = 0;
-
   int full_digit = 0;
-  if (end != 0) {
-    for (;start <= end && first_digit == 0; start++) {
-      first_digit = std::isdigit(line[start]) ? line[start] - '0' : getSpelledDigit(start, line);
-    }
 
-    for (;end > start && second_digit == 0; end--) {
+  if (end != 0) {
+    for (; start <= end && first_digit == 0; start++)
+      first_digit = std::isdigit(line[start]) ? line[start] - '0' : getSpelledDigit(start, line);
+
+    for (; end > start && second_digit == 0; end--)
       second_digit = std::isdigit(line[end]) ? line[end] - '0' : getSpelledDigit(end, line);
-    }
 
     full_digit = first_digit != 0 && second_digit != 0 ? first_digit*10 + second_digit : 0;
   }
+
+  std::cout << line << " - " << full_digit << std::endl;
 
   return full_digit;
 }
